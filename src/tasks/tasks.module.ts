f302a5da443dc890ca09166b0624998af94ce4be
Hardik Kathuria <hardikkathuria0402@gmail.com>
2025-03-18 23:19:53 +0530

    add persistence with postgres

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { Task } from './task.entity';
+import { TasksRepository } from './tasks.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])], // Register Task entity
+  providers: [TasksService, TasksRepository], // Register TasksRepository as a provider
   controllers: [TasksController],
-  providers: [TasksService],
+  exports: [TasksRepository], // Export TasksRepository if needed elsewhere
 })
 export class TasksModule {}
